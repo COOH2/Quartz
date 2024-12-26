bb1b200ccb2e2645c96ba5a7333f6f5a95c43745
COOH2 <63401527+COOH2@users.noreply.github.com>
2024-12-26 22:30:40 +0900

    Quartz sync: Dec 26, 2024, 10:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Mitr Regular 400",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
