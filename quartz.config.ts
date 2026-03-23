64285a9e17aa7c5b47131cb4fe0012d39e7596d4
COOH2 <63401527+COOH2@users.noreply.github.com>
2026-03-23 16:33:00 +0900

    Update book list, research info and header font

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Smiley Sans",
+        header: "Futura",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
